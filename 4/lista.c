abed5231fd750d5272c7a1d6edc61678be7ab4b6
João Victor Morais <jv.moraiscb@gmail.com>
2022-05-27 02:22:43 -0300

    Update lista.c

--- 4/lista.c
+++ 4/lista.c
@@ -5,85 +5,81 @@
 
 struct lista{
     Aluno* aluno;
-    Lista* next;
+    Lista* prox;
 };
 
 struct sentinela{
-    Lista* first;
-    Lista* last;
+    Lista* primeiro;
+    Lista* ultimo;
 };
 
 Sentinela* Constroi_sentinela(){
     Sentinela* sentinela = malloc(sizeof(Sentinela));
-    sentinela->first = NULL;
-    sentinela->last = NULL;
+    sentinela->primeiro = NULL;
+    sentinela->ultimo = NULL;
     return sentinela;
 }
 Lista* Constroi_lista(Aluno* aluno){
     Lista* lista = malloc(sizeof(Lista));
     lista->aluno = aluno;
-    lista->next = NULL;
+    lista->prox = NULL;
     return lista;
 }
 void Insere_aluno(Sentinela* sentinela, Aluno* aluno){
-    Lista* lista_temp = NULL;
-    if(sentinela->first == NULL){
-        sentinela->first = Constroi_lista(aluno);
-        sentinela->last = sentinela->first;
-    }
+    /*CASO SEJA O PRIMEIRO DA LISTA*/
+    if(sentinela->primeiro == NULL){
+        sentinela->primeiro = Constroi_lista(aluno);
+        sentinela->ultimo = sentinela->primeiro;
+    }  
     else{
-        sentinela->last = Constroi_lista(aluno);
-        lista_temp = sentinela->first;
-        while(lista_temp != NULL){
-            if(lista_temp->next == NULL){
-                lista_temp->next = sentinela->last;
-                break;
-            }
-            lista_temp = lista_temp->next;
-        }
+        (sentinela->ultimo)->prox = Constroi_lista(aluno);
+        sentinela->ultimo = (sentinela->ultimo)->prox;
     }
 }
 void Retira_aluno(Sentinela* sentinela, char* nome){
-    Lista* lista_temp = sentinela->first;
+    Lista* lista_temp = sentinela->primeiro;
     Lista* lista_temp_anterior = NULL;
     while(lista_temp != NULL && strcmp(Retorna_nome(lista_temp->aluno), nome) != 0){
         lista_temp_anterior = lista_temp;
-        lista_temp = lista_temp->next;
+        lista_temp = lista_temp->prox;
     }
     if(strcmp(Retorna_nome(lista_temp->aluno), nome) == 0){
-        if(lista_temp_anterior == NULL){ //PRIMEIRO DA LISTA
-            sentinela->first = lista_temp->next;
+        /*CASO SEJA O PRIMEIRO DA LISTA*/  
+        if(lista_temp_anterior == NULL){
+            sentinela->primeiro = lista_temp->prox;
             Libera_lista(lista_temp);
         }
-        else if(lista_temp->next == NULL){ //ULTIMO DA LISTA
-            sentinela->last = lista_temp_anterior;
-            lista_temp_anterior->next = NULL;
+        /*CASO SEJA O ULTIMO DA LISTA*/
+        else if(lista_temp->prox == NULL){
+            sentinela->ultimo = lista_temp_anterior;
+            lista_temp_anterior->prox = NULL;
             Libera_lista(lista_temp);
         }
+        /*CASO SEJA ENTRE*/
         else{
-            lista_temp_anterior->next = lista_temp->next;
+            lista_temp_anterior->prox = lista_temp->prox;
             Libera_lista(lista_temp);
         }
     }
 }
 void Imprime_aluno(Sentinela* sentinela){
-    Lista* lista_temp = sentinela->first;
+    Lista* lista_temp = sentinela->primeiro;
     if(lista_temp == NULL){
         printf("-\n");
     }
     while(lista_temp != NULL){
         printf("%s\n", Retorna_nome(lista_temp->aluno));
-        lista_temp = lista_temp->next;
+        lista_temp = lista_temp->prox;
     }
     printf("\n");
 }
 void Libera_sentinela(Sentinela* sentinela){
-    Lista* lista_temp = sentinela->first;
-    Lista* lista_temp_next;
+    Lista* lista_temp = sentinela->primeiro;
+    Lista* lista_temp_prox;
     while(lista_temp != NULL){
-        lista_temp_next = lista_temp->next;
+        lista_temp_prox = lista_temp->prox;
         Libera_lista(lista_temp);
-        lista_temp = lista_temp_next;
+        lista_temp = lista_temp_prox;
     }
     free(sentinela);
 }
